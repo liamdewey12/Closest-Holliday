aa0b0df659acdc90120f43d3a0072bafd012ac6e
Liam Platt Dewey <liamplattdewey@Liams-MBP.attlocal.net>
2020-03-02 22:45:07 -0800

    final changes (link)

--- app.js
+++ app.js
@@ -24,27 +24,25 @@ function closestHoliday() {
   }
   var closestNumb = Math.min.apply(Math, difference);
   var closest = difference.indexOf(closestNumb);
-  var link = "";
     switch (closest) {
     case 0:
-      link = "https://i.ytimg.com/vi/mOaciULGOa0/maxresdefault.jpg";
+       document.body.style.backgroundImage = "url('https://i.ytimg.com/vi/mOaciULGOa0/maxresdefault.jpg')";
       break;
     case 1:
-      link = "https://images.indianexpress.com/2018/02/94.jpg";
+       document.body.style.backgroundImage = "url('https://images.indianexpress.com/2018/02/94.jpg')";
       break;
     case 2:
-      link = "https://www.gannett-cdn.com/-mm-/0fd900cae7641af31d0236bc2814bd3b268ef9b1/c=0-215-1732-1517/local/-/media/2018/07/02/SNJGroup/Vineland/636661463093247105-FourthofJuly.jpg?width=534&height=401&fit=crop";
+       document.body.style.backgroundImage ="url('https://lh3.googleusercontent.com/proxy/4cftV2-v7sjQ2zxuTtl8rvmxyFykrVAWGedNm1VE3GLPKoX4RKF6vuTSfmd9vLfFsONBljzDRRmxK8JR5hkKo9Q5cGosgD5W49oMThoV8XfF0Jotz9LeOfoOBlKB')";
       break;
     case 3:
-     link = "https://i2.wp.com/www.cfrpc.org/wp-content/uploads/2015/11/new-year.jpg?fit=500%2C333&ssl=1";
+      document.body.style.backgroundImage = "url('https://i2.wp.com/www.cfrpc.org/wp-content/uploads/2015/11/new-year.jpg?fit=500%2C333&ssl=1')";
       break;
     case 4:
-      link = "https://static.vecteezy.com/system/resources/previews/000/509/139/original/halloween-pumpkins-and-dark-castle-on-background-happy-halloween-message-design-illustration-vector.jpg";
+       document.body.style.backgroundImage ="url('https://image.freepik.com/free-vector/orange-halloween-banner-with-pumpkin-spider-bats_1017-21309.jpg')";
       break;
     case 5:
-      link = "https://i.ytimg.com/vi/MT3XwJcwng8/maxresdefault.jpg";
+    document.body.style.backgroundImage = "url('https://i.ytimg.com/vi/MT3XwJcwng8/maxresdefault.jpg')";
     }
- document.body.style.backgroundImage = link;
 
 
 }
